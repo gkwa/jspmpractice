d95ac62ef5b9a7eb95f1c303c413d3818eacbac2
taylormonacelli <tailor@u.washington.edu>
2015-08-01 21:10:56 -0700

    jspm install lodash=npm:lodash

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
 var $ = require('jquery');
-console.log($.fn.jquery); // returns the current version of jQuery running
+var _ = require('lodash');
+
+console.log($.fn.jquery);
+console.log(_.VERSION);
